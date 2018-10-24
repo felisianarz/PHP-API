dd3c18137bf798f8431b1483ff4f3f8c40fc895b
Felisiana Ardelia Azzahra 34 <felisianaaraz31@gmail.com>
2018-10-24 10:07:11 +0700

    PHP-API

--- hoki.php
+++ hoki.php
@@ -5,7 +5,7 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 $level = $_POST['level'];
 $fullname = $_POST['fullname'];
-$sql_insert="INSERT INTO tabel VALUES ('$id','$username','$password','$level', '$fullname')";
+$sql_insert="INSERT INTO tabel_kelas VALUES ('$id','$username','$password','$level', '$fullname')";
 mysqli_query($conn,$sql_insert)or die(mysqli_error($conn));
 echo '<script>
 alert("Data Berhasil Disimpan");
